28f260e33dc56b3652fb7b84c8c57310d04277ef
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-06 00:35:24 +0300

    fix: correct eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,18 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
+  overrides: {
+    rules: {
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+    },
+  },
 };
