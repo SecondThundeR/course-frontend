f630e5a8593bb093eb923e6683134e8a842b3bad
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-06 00:35:19 +0300

    feat: add alias support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': resolve(__dirname, './src'),
+      '@/components': resolve(__dirname, './src/components'),
+      '@/constants': resolve(__dirname, './src/constants'),
+      '@/hooks': resolve(__dirname, './src/hooks'),
+      '@/pages': resolve(__dirname, './src/pages'),
+      '@/router': resolve(__dirname, './src/router'),
+    },
+  },
 });
