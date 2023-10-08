68eac1385db5dea042fbb74faa40232a093f29b8
SecondThundeR <awayfromgalaxy@gmail.com>
2023-10-08 15:26:51 +0300

    feat: add support for Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import '@mantine/core/styles.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import { memo } from 'react';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './router';
 import { theme } from './theme';
 
+const client = new ApolloClient({
+  uri: 'localhost:3000/graphql',
+  cache: new InMemoryCache(),
+});
+
 const App = memo(() => (
-  <MantineProvider theme={theme}>
-    <Router />
-  </MantineProvider>
+  <ApolloProvider client={client}>
+    <MantineProvider theme={theme}>
+      <Router />
+    </MantineProvider>
+  </ApolloProvider>
 ));
 
 export default App;
