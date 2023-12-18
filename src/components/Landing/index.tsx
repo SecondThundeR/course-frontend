9c736b14e03185efe82d3dafb1aee160c75a3eb0
SecondThundeR <awayfromgalaxy@gmail.com>
2023-12-18 21:09:00 +0300

    fix(components): update `Landing`
    Add missing CSS import for katex
    Fix Vite's error for export

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,3 +1,5 @@
+import 'katex/dist/katex.min.css';
+
 import { memo } from 'react';
 import {
   Container,
@@ -138,12 +140,13 @@ const LandingKatex = memo(function LandingKatex() {
   );
 });
 
-export const Landing = Object.assign(
-  {},
-  {
-    FAQ: LandingFAQ,
-    Features: LandingFeatures,
-    Hero: LandingHero,
-    Katex: LandingKatex,
-  }
-);
+const LandingBase = memo(function LandingBase() {
+  throw new Error("Doesn't use this `Landing` component. Export other components via dot notation");
+});
+
+export const Landing = Object.assign(LandingBase, {
+  FAQ: LandingFAQ,
+  Features: LandingFeatures,
+  Hero: LandingHero,
+  Katex: LandingKatex,
+});
