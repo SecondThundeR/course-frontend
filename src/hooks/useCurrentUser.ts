63c17d9f715dde030bd6c76619c602954ce3a2ef
SecondThundeR <awayfromgalaxy@gmail.com>
2023-12-21 19:44:31 +0300

    fix(hooks): add logic when API call is unauthorized
    Reset all data, tokens and conversations
    Navigate user to login route

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -2,16 +2,17 @@ import { useCallback, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLazyQuery } from '@apollo/client';
 
-import { ROOT_ROUTE } from '@/constants/routes';
+import { LOGIN_ROUTE, ROOT_ROUTE } from '@/constants/routes';
 import { CURRENT_USER } from '@/constants/queries';
 
-import { useTokensStore, useUserStore } from '@/store';
+import { useConversationsStore, useTokensStore, useUserStore } from '@/store';
 
 export default function useCurrentUser() {
   const navigate = useNavigate();
   const [getUser] = useLazyQuery(CURRENT_USER);
   const { userData, setUserData, resetUserData } = useUserStore();
   const { accessToken, resetTokens } = useTokensStore();
+  const { resetConversations } = useConversationsStore();
 
   const getUserData = useCallback(async () => {
     if (!accessToken) {
@@ -26,11 +27,18 @@ export default function useCurrentUser() {
       },
     });
 
-    if (!res || !res.data) throw new Error('Failed to fetch user data');
+    if (res?.error?.message === 'Unauthorized') {
+      resetUserData();
+      resetTokens();
+      resetConversations();
+      return navigate(LOGIN_ROUTE);
+    }
 
-    const { __typename, ...currUser } = res.data.currentUser;
+    if (!res || !res.data) {
+      throw new Error('Failed to fetch user data');
+    }
 
-    setUserData(currUser);
+    setUserData(res.data.currentUser);
   }, []);
 
   const onSignout = useCallback(() => {
