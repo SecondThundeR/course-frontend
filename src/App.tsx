71d95d4c2c09d1ab58215fd0814c0795b8d8a74b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-12-21 23:13:54 +0300

    feat: add support for Websocket endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 import '@mantine/core/styles.css';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { memo } from 'react';
 import { MantineProvider } from '@mantine/core';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
 
 import { Router } from './router';
 import { theme } from './theme';
+import { Kind, OperationTypeNode } from 'graphql';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:3000/graphql',
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === Kind.OPERATION_DEFINITION &&
+      definition.operation === OperationTypeNode.SUBSCRIPTION
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  link: splitLink,
   cache: new InMemoryCache({
     addTypename: false,
   }),
