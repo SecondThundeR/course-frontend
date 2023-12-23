b5476cc2bdab8865396749ec9856a17ef46f3772
SecondThundeR <awayfromgalaxy@gmail.com>
2023-12-24 01:35:02 +0300

    feat(App): add Apollo's dev messages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ import { Router } from './router';
 import { theme } from './theme';
 import { client } from './graphql';
 
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+
+if (import.meta.env.DEV) {
+  loadDevMessages();
+  loadErrorMessages();
+}
+
 const App = memo(() => (
   <ApolloProvider client={client}>
     <MantineProvider theme={theme}>
