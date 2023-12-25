939a776ef3a152d7413d492ab18a5c42500d7ad1
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-25 19:00:51 +0300

    feat(utils): add comments for `lastMessageDateFormat`

--- src/utils/lastMessageDateFormat.ts
+++ src/utils/lastMessageDateFormat.ts
@@ -6,6 +6,10 @@ export function lastMessageDateFormat(time: string | Date) {
 
   const currentDate = new Date();
 
+  /**
+   * If there are difference with year, month or subtract of days are bigger
+   * than week, show full date
+   */
   if (
     time.getFullYear() < currentDate.getFullYear() ||
     time.getMonth() < currentDate.getMonth() ||
@@ -14,9 +18,11 @@ export function lastMessageDateFormat(time: string | Date) {
     return `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}`;
   }
 
+  // If dates aren't equal, show date string
   if (time.getDate() < currentDate.getDate()) {
     return DAYS_MAP[time.getDay()];
   }
 
+  // Otherwise, re-use `timeFormat`
   return timeFormat(time);
 }
