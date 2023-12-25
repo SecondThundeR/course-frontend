e7f8cd50408455669b177eca87720e862b7067d8
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-25 20:29:44 +0300

    feat(components): update `Input`
    Add `isLoading` prop
    Update `onSubmit` prop

--- src/components/Chat/Input/index.tsx
+++ src/components/Chat/Input/index.tsx
@@ -6,24 +6,44 @@ import { useChatInputForm } from '@/hooks';
 
 import classes from './Input.module.css';
 
-type InputProps = { onSubmit: (message: string, isLatex: boolean) => void };
+type InputProps = {
+  isLoading: boolean;
+  onSubmit: (message: string, isLatex: boolean) => Promise<void>;
+};
 
-export const Input = memo(function Input({ onSubmit }: InputProps) {
+export const Input = memo(function Input({ isLoading, onSubmit }: InputProps) {
   const form = useChatInputForm();
 
   return (
     <form
-      onSubmit={form.onSubmit((values) => onSubmit(values.message, values.isLatex))}
+      onSubmit={form.onSubmit(async (values) => {
+        await onSubmit(values.message, values.isLatex);
+        form.reset();
+      })}
       className={classes.input}
     >
       <Flex p="md" direction="column" gap="xs">
         <Flex gap="md" align="center">
-          <TextInput w="100%" placeholder="Введите сообщение" {...form.getInputProps('message')} />
-          <ActionIcon type="submit" size="36" disabled={form.values.message.length === 0}>
+          <TextInput
+            w="100%"
+            placeholder="Введите сообщение"
+            {...form.getInputProps('message')}
+            disabled={isLoading}
+          />
+          <ActionIcon
+            type="submit"
+            size="36"
+            disabled={form.values.message.length === 0}
+            loading={isLoading}
+          >
             <IconSend className={classes.message__send} />
           </ActionIcon>
         </Flex>
-        <Checkbox label="Отправить как LaTeX сообщение" {...form.getInputProps('isLatex')} />
+        <Checkbox
+          label="Отправить как LaTeX сообщение"
+          {...form.getInputProps('isLatex')}
+          disabled={isLoading}
+        />
       </Flex>
     </form>
   );
