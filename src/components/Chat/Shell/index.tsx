ad35f96d38fb42b38aafa5f8d8cfbd39a5ca2279
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-25 20:54:05 +0300

    feat(components): update `Shell`
    Now title in header differs on mobile

--- src/components/Chat/Shell/index.tsx
+++ src/components/Chat/Shell/index.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
-import { useDisclosure } from '@mantine/hooks';
-import { AppShell, Flex, Group, Burger, Title, ScrollArea } from '@mantine/core';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+import { AppShell, Flex, Group, Burger, Title, ScrollArea, em } from '@mantine/core';
 import { Outlet, useParams } from 'react-router-dom';
 
 import { SearchInput } from '@/components/SearchInput';
@@ -11,12 +11,24 @@ import { useCurrentUser, useModal } from '@/hooks';
 import { CreateModal } from '../CreateModal';
 import { List } from '../List';
 import { UserFooter } from '../UserFooter';
+import { useConversationsStore } from '@/store';
+import { extractFullName } from '@/utils/extractFullName';
 
 export const Shell = memo(function Shell() {
   const { userData, onSignout } = useCurrentUser();
   const { modalOpened, onOpen, onClose } = useModal();
   const [opened, { toggle }] = useDisclosure();
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
   const { chatId } = useParams();
+  const conversations = useConversationsStore((state) => state.conversations);
+  const currentChatParticipant = conversations
+    .filter((conversation) => conversation.id === chatId)
+    .at(0)
+    ?.participants.filter((participant) => participant.id !== userData?.id)
+    .at(0);
+  const participantFullName = !currentChatParticipant
+    ? null
+    : extractFullName(currentChatParticipant.firstname, currentChatParticipant.lastname);
 
   return (
     <>
@@ -29,7 +41,18 @@ export const Shell = memo(function Shell() {
           <Flex h="100%" px="md" gap="md" justify="space-between" align="center">
             <Group>
               <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-              <Title order={3}>Чаты</Title>
+              <Flex direction="column">
+                {!isMobile && <Title order={3}>Чаты</Title>}
+                {isMobile && (
+                  <Title order={4}>
+                    {opened || (!opened && !participantFullName)
+                      ? 'Чаты'
+                      : participantFullName
+                        ? `Чат с ${participantFullName}`
+                        : null}
+                  </Title>
+                )}
+              </Flex>
             </Group>
             <ThemeToggle />
           </Flex>
