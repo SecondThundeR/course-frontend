4196f4a5762656160460ebe73cbb25bec4b65a01
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-26 15:49:14 +0300

    chore: add support for `vite-tsconfig-paths`

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': resolve(__dirname, './src'),
-      '@/__generated__': resolve(__dirname, './src/__generated__'),
-      '@/components': resolve(__dirname, './src/components'),
-      '@/constants': resolve(__dirname, './src/constants'),
-      '@/hooks': resolve(__dirname, './src/hooks'),
-      '@/pages': resolve(__dirname, './src/pages'),
-      '@/router': resolve(__dirname, './src/router'),
-      '@/store': resolve(__dirname, './src/store'),
-      '@/utils': resolve(__dirname, './src/utils'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
