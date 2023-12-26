bc3deddb3d9b49df9350de67065fd08489a71450
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-26 19:35:24 +0300

    feat(hooks): update scrollable height calculation

--- src/hooks/useChatAffix.ts
+++ src/hooks/useChatAffix.ts
@@ -1,8 +1,10 @@
 import { useWindowScroll } from '@mantine/hooks';
 
+const SKIP_HEIGHT = 60;
+
 export default function useChatAffix() {
   const [scroll] = useWindowScroll();
-  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight - SKIP_HEIGHT;
 
   return [scroll, scrollableHeight] as const;
 }
