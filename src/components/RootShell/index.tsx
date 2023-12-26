3d2a250b12687e6be57a459076e1323a0d42eba2
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-26 17:13:37 +0300

    fix: correct root title order when on mobile

--- src/components/RootShell/index.tsx
+++ src/components/RootShell/index.tsx
@@ -1,6 +1,6 @@
 import { type PropsWithChildren } from 'react';
-import { AppShell, Flex, Title, Button } from '@mantine/core';
-import { useHeadroom } from '@mantine/hooks';
+import { AppShell, Flex, Title, Button, em } from '@mantine/core';
+import { useHeadroom, useMediaQuery } from '@mantine/hooks';
 import { Link } from 'react-router-dom';
 
 import { LOGIN_ROUTE, REGISTER_ROUTE } from '@/constants/routes';
@@ -9,12 +9,13 @@ import { ThemeToggle } from '../';
 
 function RootShell({ children }: PropsWithChildren) {
   const pinned = useHeadroom({ fixedAt: 120 });
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
 
   return (
     <AppShell header={{ height: 60, collapsed: !pinned, offset: false }}>
       <AppShell.Header>
         <Flex h="100%" px="md" align="center" justify="space-between">
-          <Title order={4}>Project Prometheus</Title>
+          <Title order={isMobile ? 6 : 4}>Project Prometheus</Title>
           <Flex gap={8}>
             <Button component={Link} to={LOGIN_ROUTE}>
               Вход
