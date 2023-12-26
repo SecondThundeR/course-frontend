55e6ee71cc11de0f8bb3fb310f6b65839d651241
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-26 16:20:51 +0300

    chore: update config files
    Move to SWC compiler
    Move some settings from current official template of Vite (React + SWC)
    Remove unused plugins for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  root: true,
+  env: { browser: true, es2024: true },
   parserOptions: {
+    ecmaVersion: 'latest',
     project: true,
   },
-  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'postcss.config.cjs', 'codegen.ts'],
-  plugins: ['@typescript-eslint'],
   extends: [
-    'plugin:react/recommended',
+    'eslint:recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
   ],
-  settings: {
-    react: {
-      version: '18',
-    },
-  },
+  ignorePatterns: ['dist', '*.cjs', '*.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/display-name': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/array-type': 'off',
     '@typescript-eslint/consistent-type-definitions': 'off',
     '@typescript-eslint/consistent-type-imports': [
