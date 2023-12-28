ac0390a6b8c3bcabe77a6f4d0dc7218a12bbfb1e
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-28 23:00:22 +0300

    refactor(hooks): update `useChatScroll`
    Add Telegram-like logic for scrolling in useEffect

--- src/hooks/useChatScroll.ts
+++ src/hooks/useChatScroll.ts
@@ -1,7 +1,13 @@
-import { useCallback } from 'react';
-import { useScrollIntoView } from '@mantine/hooks';
+import { useCallback, useEffect } from 'react';
+import { usePrevious, useScrollIntoView } from '@mantine/hooks';
 
-export default function useChatScroll() {
+import { type ConversationInfo, useUserStore } from '@/store';
+import { useChatAffix } from '.';
+
+export default function useChatScroll(lastMessage?: ConversationInfo['messages'][number]) {
+  const [scroll, scrollableHeight] = useChatAffix();
+  const previousMessage = usePrevious(lastMessage);
+  const userData = useUserStore((state) => state.userData);
   const { targetRef, scrollIntoView } = useScrollIntoView<HTMLDivElement>({
     duration: 0,
     offset: 200,
@@ -12,6 +18,25 @@ export default function useChatScroll() {
       alignment: 'end',
     });
   }, [scrollIntoView]);
+
+  useEffect(() => {
+    onScroll();
+  }, [onScroll]);
+
+  useEffect(() => {
+    const isTheSameMessage = lastMessage?.id === previousMessage?.id;
+    if (isTheSameMessage) return;
+
+    const isScrollThreshold = scroll.y >= scrollableHeight;
+    const lastMessageAuthor = lastMessage?.from?.id;
+    const isMessageFromCurrentUser = lastMessageAuthor === userData?.id;
+    const isMessageFromOtherUser = lastMessageAuthor !== userData?.id;
+
+    if (isMessageFromCurrentUser || (isMessageFromOtherUser && isScrollThreshold)) {
+      onScroll();
+      return;
+    }
+  }, [lastMessage, onScroll, previousMessage?.id, scroll.y, scrollableHeight, userData?.id]);
 
   return { targetRef, onScroll };
 }
