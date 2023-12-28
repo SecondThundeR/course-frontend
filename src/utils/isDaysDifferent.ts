6db3c4a8cd2eb4535b2a7e15600cd51d01b66966
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-28 20:42:13 +0300

    fix(utils): remove faulty comparison in `isDaysDifferent`

--- src/utils/isDaysDifferent.ts
+++ src/utils/isDaysDifferent.ts
@@ -1,5 +1,5 @@
 export function isDaysDifferent(createdAt1: string | undefined, createdAt2: string) {
-  if (!createdAt1 || createdAt1 === createdAt2) return true;
+  if (!createdAt1) return true;
 
   const createdAtFirst = new Date(createdAt1);
   const createdAtSecond = new Date(createdAt2);
