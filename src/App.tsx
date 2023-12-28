594560156ec4cc731e1e8af0bcb0077e3b022305
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-28 19:11:33 +0300

    style(App): reorder imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import '@mantine/core/styles.css';
 import { memo } from 'react';
 import { MantineProvider } from '@mantine/core';
 import { ApolloProvider } from '@apollo/client';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 
 import { Router } from './router';
 import { theme } from './theme';
 import { client } from './graphql';
-
-import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 
 if (import.meta.env.DEV) {
   loadDevMessages();
