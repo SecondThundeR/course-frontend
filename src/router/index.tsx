deae32e3c473a0fd4aef301329863417aa23b5b0
Uladzislau Hramyka <awayfromgalaxy@gmail.com>
2023-12-28 19:46:44 +0300

    fix(router): add `index: true` for `LazyHome`

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: ROOT_ROUTE,
+        index: true,
         element: (
           <Suspense>
             <LazyHome />
